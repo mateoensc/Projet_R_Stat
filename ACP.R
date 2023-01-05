e277078f3cccaaafee601d028e8b9ae31f3a29b3
Mateo Moreau <mmoreau016@bordeaux-inp.fr>
2023-01-05 18:48:34 +0100

    :zap:

--- ACP.R
+++ ACP.R
@@ -12,9 +12,9 @@ corrplot(Matricecor,type="upper", order="hclust",tl.col = "black",tl.srt = 45)
 # Mise en oeuvre de l’ACP
 #----------------------------------------
 # On enlève la variable à expliquer PctBf
-res <- PCAmix(donneesProjet[,2:14], graph = FALSE)
-res
-round(res$eig,digit=2) # permet d’afficher les valeurs propres et les pourcentages
+acp <- PCAmix(donneesProjet[,2:14], graph = FALSE, ndim = 13)
+acp
+round(acp$eig,digit=2) # permet d’afficher les valeurs propres et les pourcentages
 # de variances expliquees par chaque axe
 # On applique le critère de Kaiser
 # eigen value > 1
@@ -22,27 +22,27 @@ round(res$eig,digit=2) # permet d’afficher les valeurs propres et les pourcent
 # On retiendra dim1, dim2, dim3 totalité de la part de variance expliqué 
 # 61.93 + 11.20 + 6.84 = 79.97 %
 
-# Graphique de l’ebouli des valeurs propres
-barplot(res$eig[,1],main="Eigenvalues",names.arg=1:nrow(res$eig))
+# Graphique de l’ebouli des valeurs propacp
+barplot(acp$eig[,1],main="Eigenvalues",names.arg=1:nrow(acp$eig))
 abline(h=1,col=2,lwd=2)
 
 #permet d’afficher la fenetre d’aide de la commande "plot.PCA"
-plot(res,axes=c(1,2),choice="ind", label = TRUE) # on retrouve ici le graphique des individus (plan 1-2)
-plot(res,axes=c(1,2),choice="cor") # on retrouve ici le cercle des corrélations
+plot(acp,axes=c(1,2),choice="ind", label = TRUE) # on retrouve ici le graphique des individus (plan 1-2)
+plot(acp,axes=c(1,2),choice="cor") # on retrouve ici le cercle des corrélations
 # des variables (plan 1-2)
-plot(res,axes=c(2,3),choice="ind", label = TRUE)
-plot(res,axes=c(2,3),choice="cor", label = TRUE)
-plot(res,axes=c(1,2),choice="sqload") # on retrouve ici le graphique des "square loadings" (plan 1-2)
+plot(acp,axes=c(2,3),choice="ind", label = TRUE)
+plot(acp,axes=c(2,3),choice="cor", label = TRUE)
+plot(acp,axes=c(1,2),choice="sqload") # on retrouve ici le graphique des "square loadings" (plan 1-2)
 
 #--------------------------------------------------------------------
 # Sorties numeriques pour les individus et les variables
 #--------------------------------------------------------------------
-res$ind # permet d’afficher l’ensemble des sorties numeriques associees aux individus :
-# coordonnees, contributions, cosinus carres
-round(res$ind$cos2,digit=3) # uniquement les cosinus carres
-res$quanti # permet d’afficher l’ensemble des sorties numeriques associees aux variables :
-# coordonnees, contributions, cosinus carres
-round(res$quanti$cos2,digit=3) # uniquement les cosinus carres
+acp$ind # permet d’afficher l’ensemble des sorties numeriques associees aux individus :
+# coordonnees, contributions, cosinus caracp
+round(acp$ind$cos2,digit=3) # uniquement les cosinus caracp
+acp$quanti # permet d’afficher l’ensemble des sorties numeriques associees aux variables :
+# coordonnees, contributions, cosinus caracp
+round(acp$quanti$cos2,digit=3) # uniquement les cosinus caracp
 # Confirme que les variables sont corrélées
-# Variable âge intéressante ??
+# Variable âge intéacpsante ??
 # Y peut-être reconstruit ??
--- Regressionlinéairemultiple.R
+++ Regressionlinéairemultiple.R
@@ -1,6 +1,7 @@
-# Test de la régression avec deux variables fortement corrélées
+# Variable à expliquer
 y = donneesProjet$Pct.BF # Pct Bf
-
+composantes = acp$ind$coord # Ici charger l'ACP
+# Test de la régression avec deux variables fortement corrélées
 test = lm(y~donneesProjet$Weight)
 summary(test)
 # 0.9326905 valeur de corrélation entre Hip et Weight
@@ -8,7 +9,7 @@ test2 = lm(y~donneesProjet$Weight+donneesProjet$Hip)
 summary(test2)
 
 # Test de la regression linéaire sur les trois premières composantes
-composantes = res$ind$coord # Ici charger l'ACP
+
 res3 <- lm(y~composantes[,1]) # Par rapport à la première composante
 summary(res3)
 res4 <- lm(y~composantes[,1]+composantes[,2]+composantes[,3])
@@ -19,8 +20,6 @@ summary(res5)
 # Et donc de gagner en précision du modèle
 resF <- lm(y~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4])
 summary(resF)
-# On observe que rajouter la 4eme composante permet d'augmenter le R ajusté
-# Et donc de gagner en précision du modèle
 
 # Etude des résidus
 par(mfrow=c(1,2))
@@ -31,18 +30,18 @@ abline(0,1,col=2)
 
 
 
-shapiro.test(resF$residuals)
+shapiro.test(resF$residuals) # On ne rejette pas H0
 
 
 # Affinage du modèle avec les composantes principales
-matYX<-data.frame(y,composantes[,1],composantes[,2],composantes[,3],composantes[,4],composantes[,5])
+matYX<-data.frame(y,composantes[,1],composantes[,2],composantes[,3],composantes[,4],composantes[,5],composantes[,6]+composantes[,7]+composantes[,8]+composantes[,9]+composantes[,10]+composantes[,11]+composantes[,12]+composantes[,13]) # modele initial complet
 res <- lm(y~1,data=matYX) 
-step(res,~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4]+composantes[,5])  
-step(res,~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4],trace=F)
-# Selectionne bien les 4 composantes principales
-res <- lm(y~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4]+composantes[,5],data=matYX) # modele initial complet
+step(res,~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4]+composantes[,5]+composantes[,6]+composantes[,7]+composantes[,8]+composantes[,9]+composantes[,10]+composantes[,11]+composantes[,12]+composantes[,13])  
+
+# Selectionne 1,2,3,4,12,7,6,10,8,11
+res <- lm(y~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4]+composantes[,5]+composantes[,6]+composantes[,7]+composantes[,8]+composantes[,9]+composantes[,10]+composantes[,11]+composantes[,12]+composantes[,13],data=matYX) # modele initial complet
 step(res)  
-# Seule la 5eme est enlevée
+# Selectionne 1,2,3,4,6,7,8,10,11,12
 # Selection des variables sans les composantes principales
 matYX2<-data.frame(y,donneesProjet[,2],donneesProjet[,3],donneesProjet[,4],donneesProjet[,5],donneesProjet[,6],donneesProjet[,7],donneesProjet[,8],donneesProjet[,9],donneesProjet[,10],donneesProjet[,11],donneesProjet[,12],donneesProjet[,13],donneesProjet[,14])
 res <- lm(y~1,data=matYX2) 
@@ -50,17 +49,19 @@ step(res,~donneesProjet[,2]+donneesProjet[,3]+donneesProjet[,4]+donneesProjet[,5
 step(res,~donneesProjet[,2]+donneesProjet[,3]+donneesProjet[,4]+donneesProjet[,5]+donneesProjet[,6]+donneesProjet[,7]+donneesProjet[,8]+donneesProjet[,9]+donneesProjet[,10]+donneesProjet[,11]+donneesProjet[,12]+donneesProjet[,13]+donneesProjet[,14],trace=F)
 # Ajout des variables avec le modèle sans variable explicative
 # Weight, Abdomen, Wrist, Bicep, Age, Thigh
-res_selec <- lm(y~donneesProjet$Weight+donneesProjet$Abdomen+donneesProjet$Wrist+donneesProjet$Bicep+donneesProjet$Age+donneesProjet$Thigh)
-summary(res_selec)
+
 res <- lm(y~donneesProjet[,2]+donneesProjet[,3]+donneesProjet[,4]+donneesProjet[,5]+donneesProjet[,6]+donneesProjet[,7]+donneesProjet[,8]+donneesProjet[,9]+donneesProjet[,10]+donneesProjet[,11]+donneesProjet[,12]+donneesProjet[,13]+donneesProjet[,14],data=matYX) # modele initial complet
 step(res)  
 # Age, Height, Neck, Hip, Thigh, Wrist, Forearm
 summary(res)
 
-# on enlève âge qui paraît moins corrélé avec les autres
-res<-lm(y~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4],data=matYX)
-drop1(res)
-
+# Comparaison entre deux modèles celui avec composantes principales et celui avec variables explicatives
+# Avec ACP 
+res_acp <- lm(y~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4]+composantes[,6]+composantes[,7]+composantes[,8]+composantes[,10]+composantes[,11]+composantes[,12])
+summary(res_acp)
+# Variables explicatives
+res_selec <- lm(y~donneesProjet$Weight+donneesProjet$Abdomen+donneesProjet$Wrist+donneesProjet$Bicep+donneesProjet$Age+donneesProjet$Thigh)
+summary(res_selec)
 
 
 bartlett.test(donneesProjet[,1]~donneesProjet[,2])
