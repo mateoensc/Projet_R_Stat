11bb7419d033167e012235ece1e0583de17d820f
Mateo Moreau <mmoreau016@bordeaux-inp.fr>
2023-01-05 16:58:04 +0100

    :zap:

--- Regressionlinéairemultiple.R
+++ Regressionlinéairemultiple.R
@@ -29,29 +29,32 @@ abline(h=0,col=2)
 plot(resF$fitted,y)
 abline(0,1,col=2)
 
-par(mfrow=c(1,1))
-plot(rstudent(resF),ylab="res. student.",ylim=c(-3.5,3.5)) 
-abline(h=c(-2,0,2),lty=c(2,1,2)) 
+
 
 shapiro.test(resF$residuals)
 
 
-# Affinage du modèle
-matYX<-data.frame(y,composantes[,1],composantes[,2],composantes[,3],composantes[,4])
+# Affinage du modèle avec les composantes principales
+matYX<-data.frame(y,composantes[,1],composantes[,2],composantes[,3],composantes[,4],composantes[,5])
 res <- lm(y~1,data=matYX) 
-step(res,~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4])  
+step(res,~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4]+composantes[,5])  
 step(res,~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4],trace=F)
-
-res <- lm(y~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4],data=matYX) # modele initial complet
+# Selectionne bien les 4 composantes principales
+res <- lm(y~composantes[,1]+composantes[,2]+composantes[,3]+composantes[,4]+composantes[,5],data=matYX) # modele initial complet
 step(res)  
-
+# Seule la 5eme est enlevée
+# Selection des variables sans les composantes principales
 matYX2<-data.frame(y,donneesProjet[,2],donneesProjet[,3],donneesProjet[,4],donneesProjet[,5],donneesProjet[,6],donneesProjet[,7],donneesProjet[,8],donneesProjet[,9],donneesProjet[,10],donneesProjet[,11],donneesProjet[,12],donneesProjet[,13],donneesProjet[,14])
 res <- lm(y~1,data=matYX2) 
 step(res,~donneesProjet[,2]+donneesProjet[,3]+donneesProjet[,4]+donneesProjet[,5]+donneesProjet[,6]+donneesProjet[,7]+donneesProjet[,8]+donneesProjet[,9]+donneesProjet[,10]+donneesProjet[,11]+donneesProjet[,12]+donneesProjet[,13]+donneesProjet[,14])  
 step(res,~donneesProjet[,2]+donneesProjet[,3]+donneesProjet[,4]+donneesProjet[,5]+donneesProjet[,6]+donneesProjet[,7]+donneesProjet[,8]+donneesProjet[,9]+donneesProjet[,10]+donneesProjet[,11]+donneesProjet[,12]+donneesProjet[,13]+donneesProjet[,14],trace=F)
-
+# Ajout des variables avec le modèle sans variable explicative
+# Weight, Abdomen, Wrist, Bicep, Age, Thigh
+res_selec <- lm(y~donneesProjet$Weight+donneesProjet$Abdomen+donneesProjet$Wrist+donneesProjet$Bicep+donneesProjet$Age+donneesProjet$Thigh)
+summary(res_selec)
 res <- lm(y~donneesProjet[,2]+donneesProjet[,3]+donneesProjet[,4]+donneesProjet[,5]+donneesProjet[,6]+donneesProjet[,7]+donneesProjet[,8]+donneesProjet[,9]+donneesProjet[,10]+donneesProjet[,11]+donneesProjet[,12]+donneesProjet[,13]+donneesProjet[,14],data=matYX) # modele initial complet
 step(res)  
+# Age, Height, Neck, Hip, Thigh, Wrist, Forearm
 summary(res)
 
 # on enlève âge qui paraît moins corrélé avec les autres
